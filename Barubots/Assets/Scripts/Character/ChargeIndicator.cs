9d30447af669fd346c6d8dd5244ac3379c433700
Sly Emenike <sly.emenike@hva.nl>
2018-07-15 01:54:35 +0200

    Added full ring charge indicator, working for multiple players.

---
+++ Barubots/Assets/Scripts/Character/ChargeIndicator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargeIndicator : MonoBehaviour {
+
+    private Renderer _renderer;
+    private MaterialPropertyBlock _propBlock;
+
+    void Awake()
+    {
+        _propBlock = new MaterialPropertyBlock();
+        _renderer = GetComponent<Renderer>();
+    }
+
+    public void SetValue(float value)
+    {
+        _renderer.GetPropertyBlock(_propBlock);
+        _propBlock.SetFloat("_AlphaValue", 1 - value);
+        _renderer.SetPropertyBlock(_propBlock);
+    }
+}
