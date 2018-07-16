53ca50e00eb37e42b1d5859f7673727fdfdb5c09
Sly Emenike <sly.emenike@hva.nl>
2018-07-16 23:19:13 +0200

    Added flash to shoot particles.

--- Barubots/Assets/Scripts/Projectile/Projectile.cs
+++ Barubots/Assets/Scripts/Projectile/Projectile.cs
@@ -64,7 +64,7 @@ public class Projectile : MonoBehaviour
         myCollider.enabled = true;
         currentLifeTime = lifeTime;
         isActive = true;
-        particlePrefab.Play();
+        particlePrefab.Play(true);
         myRigidbody.AddForce(force, ForceMode.Impulse);
     }
 
